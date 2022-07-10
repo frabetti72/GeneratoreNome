d1aa4f45eb961b718e4294931a3d6ec080f78b08
Roberto Frabetti <roberto.frabetti@studio.unibo.it>
2022-07-10 18:48:16 +0100

    aggiunte cose base view

--- app/src/main/java/com/example/generatorenome/view/MainActivity.kt
+++ app/src/main/java/com/example/generatorenome/view/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.generatorenome.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.generatorenome.R
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val casellaTesto: TextView= findViewById(R.id.textView)
+
+        val ourText: TextInputEditText = findViewById(R.id.ourTextBox)
+        //val ourText: TextInputEditText = R.id.ourTextBox
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            onButtonClick()
+        }
+
+    }
+
+    override fun onButtonClick(){
+
     }
 }
